9c613053276726f546f8b284311a8b97b62e9447
Sumin Lim <imsumin018@gmail.com>
2023-07-31 17:18:44 +0900

    Update digitTreeSum.py

--- aaaa/digitTreeSum.py
+++ aaaa/digitTreeSum.py
@@ -5,8 +5,7 @@
 #     self.value = x
 #     self.left = None
 #     self.right = None
-
-
+#my Solution in dfs
 def recur(node, res) :
     
     ans = 0    
@@ -34,6 +33,29 @@ def recur(node, res) :
 def solution(t):
     
     return recur (t, [])
+#
+# Definition for binary tree:
+# class Tree(object):
+#   def __init__(self, x):
+#     self.value = x
+#     self.left = None
+#     self.right = None
+def solution(t):
+    if not t:
+        return 0
     
+    stack = [(t, 0)]
+    sum = 0
+    while stack:
+        
+        cur, v = stack.pop()
+        if cur.left or cur.right:
+            if cur.left:
+                stack.append((cur.left, cur.value + v * 10))
+            if cur.right:
+                stack.append((cur.right, cur.value + v * 10))
+        else:
+            sum += cur.value + v * 10
     
+    return sum
 
