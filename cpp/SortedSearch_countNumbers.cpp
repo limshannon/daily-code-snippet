85c3642729de4efb809bcf85c5cd3a35e822954b
limshannon <imsumin018@gmail.com>
2024-05-22 13:14:03 +0900

    Update SortedSearch_countNumbers.cpp

--- cpp/SortedSearch_countNumbers.cpp
+++ cpp/SortedSearch_countNumbers.cpp
@@ -4,8 +4,19 @@
 
 int countNumbers(const std::vector<int>& sortedVector, int lessThan)
 {
-    throw std::logic_error("Waiting to be implemented");
+    auto res = std::lower_bound(std::begin(sortedVector), std::end(sortedVector), lessThan);
+    return std::distance(std::begin(sortedVector), res);
 }
+/*{ //Performance test when sortedVector contains lessThan: Time limit exceeded 
+    int res =0;
+    for(std::size_t i=0; i< sortedVector.size(); ++i)
+        {
+            if (sortedVector[i]<lessThan){
+              ++res;
+            }
+        }
+    return res;
+}*/
 
 #ifndef RunTests
 int main()
